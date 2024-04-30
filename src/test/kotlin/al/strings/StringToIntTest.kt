706f834ccb1daf42aad92e8d2c5ea891d011d6bb
Gegenbauer <865815634@qq.com>
2024-04-30 11:28:47 +0800

    增加题解： leetcode-8 字符串转换正数

--- src/test/kotlin/al/strings/StringToIntTest.kt
+++ src/test/kotlin/al/strings/StringToIntTest.kt
@@ -8,7 +8,35 @@ class StringToIntTest {
     @Test
     fun test() {
         assertEquals(42, StringToInt().myAtoi("42"))
+    }
+
+    @Test
+    fun test2() {
         assertEquals(-42, StringToInt().myAtoi("   -42"))
+    }
+
+    @Test
+    fun test3() {
         assertEquals(4193, StringToInt().myAtoi("4193 with words"))
+    }
+
+    @Test
+    fun test4() {
+        assertEquals(-2147483648, StringToInt().myAtoi("-91283472332"))
+    }
+
+    @Test
+    fun test5() {
+        assertEquals(0, StringToInt().myAtoi(""))
+    }
+
+    @Test
+    fun test6() {
+        assertEquals(2147483647, StringToInt().myAtoi("2147483648"))
+    }
+
+    @Test
+    fun test7() {
+        assertEquals(2147483646, StringToInt().myAtoi("2147483646"))
     }
 }
